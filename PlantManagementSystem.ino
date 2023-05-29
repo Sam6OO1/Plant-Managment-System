1ea48dd4205de42d464f28c840973fdbad75203e
Sam6OO1 <samuelnaude@rhs.school.nz>
2023-05-29 14:23:22 +1200

    Display messages on LCD display and serial console if temperature, humidity or moisture values are not within their recommended values

--- PlantManagementSystem.ino
+++ PlantManagementSystem.ino
@@ -3,15 +3,19 @@
 #include <SimpleDHT.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2); //Creating LCD object with i2c address, number of columns and rows.
 // Wiring: Connect SDA to a4, Connect SDL to a5
-const int pinDHT11 = 5; 
-const int pinSEN0114 = 17;
-const int pinButton = 2;
+const int pinDHT11 = 5; // Temperature and Humidity Sensor
+const int pinSEN0114 = 17; // Moisture Sensor
+const int pinButton = 2; 
 const int pinLed = LED_BUILTIN;
+const int minimumRTemperature = 18; // Minimum Recommended Temperature
+const int maximumRTemperature = 24; // Maximum Recommended Temperature
+const int minimumRHumidity = 75; // Minimum Recommended Humidity
+const int maximumRHumidity = 85; // Maximum Recommnded Humidity
 SimpleDHT11 dht11(pinDHT11);
 volatile int buttonState = 0; //Variable for button status
 
 
-void changeThing() {
+void changeThing() {      
   buttonState = digitalRead(pinButton);
   digitalWrite(pinLed, !buttonState);
 
@@ -26,6 +30,7 @@ void setup() {
   lcd.backlight(); // Turns on backlight
   lcd.setCursor(1, 0); // Sets where the text should be displayed, The first parameter shows column and second parameter shows row
   attachInterrupt(0, changeThing, CHANGE); // Attach interrupt to ISR vector
+
   
 
 
@@ -34,7 +39,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  displayData();
+  displayData(); 
  
   delay(1500);
   Serial.println();
@@ -49,7 +54,7 @@ void displayData(){
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
     Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
-    // check is dht11 is working
+    // check if dht11 is working
     return;
   }
   //print temp, moisture and humidity to serial and lcd
@@ -68,13 +73,14 @@ void displayData(){
   lcd.setCursor(9,0);
   lcd.print("M: ");
   lcd.print(soilMositure);
-  if (soilMositure < 300) {
+
+  if (soilMositure < 300) { //Print soil moisture alerts to LCD and serial display
     Serial.print(" Dry");
     lcd.setCursor(9, 1);
     lcd.print("Dry");
 
   }
-  if (300 < soilMositure && soilMositure < 700) {
+  if (300 < soilMositure && soilMositure < 700) {  
     Serial.print(" Humid");
     lcd.setCursor(9, 1);
     lcd.print("Humid");
@@ -85,7 +91,37 @@ void displayData(){
     lcd.setCursor(9, 1);
     lcd.print("Wet");
 
-}
+  }
+  if (minimumRTemperature > temperature) { //Check if temperature is below minimumRTemperature if so print to LCD and serial console
+    Serial.print("Below minimum set temperature");
+    lcd.clear();
+    lcd.print("Below minimum");
+    lcd.setCursor(0,1);
+    lcd.print("set temperature");
+  }
+  if (maximumRTemperature < temperature) { //Check if temperature is over maximumRTemperature if so print to LCD and serial console
+    Serial.print("Over maximum set temperature");
+    lcd.clear();
+    lcd.print("Over maximum");
+    lcd.setCursor(0,1);
+    lcd.print("set temperature");
+  }
+  if (minimumRHumidity > humidity) { //Check if humidity is below minimumRHumidity if so print to LCD and serial console
+    Serial.print("Below minimum set humidity");
+    lcd.clear();
+    lcd.print("Below minimum");
+    lcd.setCursor(0,1);
+    lcd.print("set humidity");
+  }
+  if (maximumRHumidity < humidity) { //Check if humidity is over maximumRHumidity if so print to LCD and serial console
+    Serial.print("Over maximum set humidity");
+    lcd.clear();
+    lcd.print("Over maximum");
+    lcd.setCursor(0,1);
+    lcd.print("set humidity");
+  }
+
+
 
 
 
